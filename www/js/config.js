842851fe0f4a82f73696d58e0a6772c4d9ca0d38
benmed00 <ben94med@gmail.com>
2017-06-08 12:42:00 +0000

    first push

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+angular.module('starter.configs', [])
+
+.constant("CONFIG", {
+    "FIREBASE_API": 'AIzaSyDZNd6htDd5chX0gtQixvqkjPO9w-eii5w',
+    "FIREBASE_AUTH_DOMAIN": 'tizi-fri.firebaseapp.com',
+    "FIREBASE_DB_URL": 'https://tizi-fri.firebaseio.com',
+    "FIREBASE_STORAGE": 'tizi-fri.appspot.com',
+    "MESSAGE_SENDER_ID": '11341206272'
+});
